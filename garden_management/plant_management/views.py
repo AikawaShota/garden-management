404b42cbfc6e5c55ff38531bd201dbc921c6255f
AikawaShota <git@a-shota.com>
2024-01-17 23:17:33 +0900

    add: Add PlantListView to views.py.

--- garden_management/plant_management/views.py
+++ garden_management/plant_management/views.py
@@ -1,6 +1,12 @@
 from django.views import generic
 from . import models
 # from . import forms
+import datetime
+
+
+def get_watering_state(last_watering_date, watering_frequency):
+    time_difference = last_watering_date + watering_frequency - datetime.datetime.now()
+    return time_difference
 
 
 # 植物一覧（水やり状態）
