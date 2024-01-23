59e9753f29e3d23fe4cef474b78040d87a558a29
AikawaShota <s-aikawa@kokua.surf>
2024-01-23 14:26:26 +0900

    add: Add custom access control mixin and custon 403page.

--- garden_management/plant_management/urls.py
+++ garden_management/plant_management/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     # 植物一覧
     path('list', views.PlantListView.as_view(), name='list'),
     # 水やり完了
-    path('watering-complete/<int:pk>', views.complete_watering, name='watering-complete')
+    path('watering-complete/<int:pk>', views.complete_watering, name='watering-complete'),
+    # 植物詳細
+    path('detail/<int:pk>', views.PlantDetailView.as_view(), name='detail'),
 ]
