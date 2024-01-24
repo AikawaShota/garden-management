1c636af8879f6660001b2514dcccb9e850d98430
AikawaShota <git@a-shota.com>
2024-01-25 00:24:25 +0900

    fix: Revised the design of the plant deletion confirmation screen.

--- garden_management/authentication/views.py
+++ garden_management/authentication/views.py
@@ -1,7 +1,6 @@
 from . import forms
 from django.urls import reverse_lazy
 from django.views import generic
-from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
 
