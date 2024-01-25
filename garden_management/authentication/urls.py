6179513a45339ab33fa0062bf37d0b900d9b7890
AikawaShota <git@a-shota.com>
2024-01-25 13:38:14 +0900

    Added redirection when accessing the root of each application.

--- garden_management/authentication/urls.py
+++ garden_management/authentication/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 app_name = 'authentication'
 
 urlpatterns = [
+    # /authenticationにアクセスした場合は、ログインページにリダイレクト。
+    path('', RedirectView.as_view(url='/authentication/login')),
     # ユーザ登録
     path('signup', views.SignUpView.as_view(), name='signup'),
     # ログイン
--- garden_management/plant_management/urls.py
+++ garden_management/plant_management/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 app_name = 'plant_management'
 
 urlpatterns = [
+    # /plant-managementにアクセスした場合は、一覧ページにリダイレクト。
+    path('', RedirectView.as_view(url='/plant-management/list')),
     # 植物一覧
     path('list', views.PlantListView.as_view(), name='list'),
     # 水やり完了
